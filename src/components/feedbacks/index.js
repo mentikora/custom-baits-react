62c4a3f46b4b450351e5b7815fc2b18047fa7a9f
Roman Horobets <roman.horobets@rolique.io>
2018-08-28 14:21:41 +0300

    feedbacks

--- src/components/feedbacks/index.js
+++ src/components/feedbacks/index.js
@@ -8,13 +8,13 @@ import image from './avatar.png';
 export class Feedbacks extends Component {
   render() {
     const settings = {
-      autoplay: true,
-      dots: false,
+      autoplay: false,
+      dots: true,
       arrows: false,
       speed: 500,
       slidesToShow: 1,
       infinite: false,
-      swipeToSlide: true
+      swipeToSlide: true,
     };
     return (
       <DataConsumer>
@@ -23,14 +23,23 @@ export class Feedbacks extends Component {
             <Slider {...settings} className="bait-colors__slider">
               {
                 feedbacks && feedbacks.map((item, key) => (
-                  <div key={key}>
-                    {
-                      <img className="feedbacks__image" src={image} alt="alt"/>
-                    }
-                    <h3 className="feedbacks__username">
-                      {item.fields.name}
-                    </h3>
-                    <MarkdownPreview value={ item.fields.text }/>
+                  <div key={key} className="feedback-item">
+                    <div className="feedback-item__header">
+                      <div className="feedback-item__header-leftside">
+                        {
+                          <img className="feedback-item__image" src={image} alt="alt"/>
+                        }
+                      </div>
+                      <div className="feedback-item__header-rightside">
+                        <h3 className="feedback-item__username">
+                          {item.fields.name}
+                        </h3>
+                        <span className="feedback-item__date">
+                          {item.fields.date}
+                        </span>
+                      </div>
+                    </div>
+                    <MarkdownPreview className="feedback-item__text" value={ item.fields.text }/>
                   </div>
                 ))
               }
